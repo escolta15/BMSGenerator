f7d72bdd91c27400d65b25e79ed05ba8ecf6b61e
Pablo Mora Herreros <escolta15@gmail.com>
2024-08-21 20:29:48 +0200

    file to execute all apps is working

--- problem_dsl.rb
+++ problem_dsl.rb
@@ -1,6 +1,7 @@
 require 'erb'
 require 'json'
 require 'tty-prompt'
+require 'fileutils'
 
 prompt = TTY::Prompt.new
 
@@ -51,6 +52,40 @@ def add_route(app_name)
   end
 end
 
+def add_start_command(app_name)
+  file_path = 'start_apps.js'
+  content = File.read(file_path)
+  regex = /(\[\s*)([^\]]*)(\s*\])/
+  new_content = content.gsub(regex) do |match|
+    opening_bracket = $1
+    array_content = $2.strip
+    closing_bracket = $3
+    new_content = "\n\t\"\\\"ng serve #{app_name}\\\"\""
+    if array_content.empty?
+      "#{opening_bracket}#{new_content}#{closing_bracket}"
+    else  
+      "#{opening_bracket}#{array_content}, #{new_content}#{closing_bracket}"
+    end
+  end
+  File.open(file_path, 'w') do |file|
+    file.write(new_content)
+  end
+end
+
+def add_file(origin_file, destination_file)
+  FileUtils.cp(origin_file, destination_file)
+end
+
+def add_script_in_package_json
+  file_path = 'package.json'
+  file_content = File.read(file_path)
+  data = JSON.parse(file_content)
+  data["scripts"]["start:all"] = "node start_apps.js"
+  File.open(file_path, 'w') do |file|
+    file.write(JSON.pretty_generate(data))
+  end
+end
+
 name = ""
 
 while name == ""
@@ -61,10 +96,12 @@ workspace_name = "BMS-" + name.gsub(" ", "_")
 puts "The name is #{workspace_name}"
 if !Dir.exist?(workspace_name)
   system("ng new #{workspace_name} --create-application=false")
+  add_file("templates/template_start_apps.js", "#{workspace_name}/start_apps.js")
 else
   puts("The workspace #{workspace_name} exists. It will not be created again.")
 end
 Dir.chdir(workspace_name)
+add_script_in_package_json
 
 options = ["Z40", "Z41"]
 selection = prompt.select("Select an option please:", options)
@@ -80,17 +117,21 @@ project_name = to_lower_kebab_case(name)
 puts "The name is #{project_name}"
 if !Dir.exist?("projects/#{project_name}")
   system("npm i @angular-architects/native-federation@17 -D")
-  system("ng generate application home --ssr=false --routing --style=scss")
+  system("npm i npm i concurrently -D")
+  system("ng generate application home --ssr=false --routing --style=scss --skip-install=true")
   system("ng g @angular-architects/native-federation:init --project home --port 4200 --type dynamic-host")
   empty_json_file('projects/home/src/assets/federation.manifest.json')
+  add_start_command('home')
   system("ng generate application #{project_name} --ssr=false --routing --style=scss --skip-install=true")
   system("ng g @angular-architects/native-federation:init --project #{project_name} --port 4201 --type remote")
   add_remote_entry(project_name, 4201)
   add_route(project_name)
+  add_start_command(project_name)
   system("ng generate application #{project_name}1 --ssr=false --routing --style=scss --skip-install=true")
   system("ng g @angular-architects/native-federation:init --project #{project_name}1 --port 4202 --type remote")
   add_remote_entry(project_name + '1', 4202)
   add_route(project_name + '1')
+  add_start_command(project_name + '1')
 else
   puts("The workspace #{project_name} exists. It will not be created again.")
 end
---
+++ templates/template_start_apps.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process');
+
+const apps = [];
+
+if (apps.length) {
+  exec(`concurrently ${apps.join(' ')}`, (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error starting the apps comand: ${err}`)
+        return;
+    }
+    console.log(stdout);
+    if (stderr) {
+        console.error(`stderr: ${stderr}`);
+    }
+  });
+} else {
+  console.error('There are not any apps yet');
+}
